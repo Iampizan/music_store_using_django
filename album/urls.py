98336ec452568bba31ab95bf148e19c09b58039b
unknown <olabiyipelumi86@gmail.com>
2026-03-16 19:24:53 +0100

    Merge remote repository

--- album/urls.py
+++ album/urls.py
@@ -6,4 +6,5 @@ app_name = 'album'
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('albums/', views.album_listing, name='album_list'),
+    path('albums/<int:album_id>/', views.album_details, name='album_details')
 ]
--- album/views.py
+++ album/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def home_page(request):
     return render(request, 'album/home.html')
+
+
 def album_listing(request):
     albums = Album.objects.select_related('artist').order_by('-release_date')
     return render(request, 'album/album_list.html', {'albums': albums})
+
+
+def album_details(request, album_id):
+    albums = Album.objects.select_related('artist')
+    album = get_object_or_404(Album, id=album_id)
+    return render(request, "album/album_detail.html", {'album': album, 'albums': albums})
