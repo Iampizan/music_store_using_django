bddb04b5ba03e7023ca956189592cea14bb37241
unknown <olabiyipelumi86@gmail.com>
2026-03-17 10:40:50 +0100

    made adjustment to the artist_details.html template

--- artists/views.py
+++ artists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Artist
+from album.models import Album
 
 def artist_list(request):
     artists = Artist.objects.all().order_by('debut_year')
@@ -7,5 +8,6 @@ def artist_list(request):
 
 def artist_details(request, artist_pk):
     artist = get_object_or_404(Artist, id=artist_pk)
-    return render(request, 'artist/artist_details.html', {'artist': artist})
+    albums = Album.objects.select_related('artist')
+    return render(request, 'artist/artist_details.html', {'artist': artist, 'albums': albums})
 
